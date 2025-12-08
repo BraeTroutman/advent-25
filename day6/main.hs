9fb4779a3036eca938a7cc1a9bc7610f81145c94
BraeTroutman <btroutma@redhat.com>
2025-12-08 14:16:16 -0500

    add day6 part 2 solution

--- day6/main.hs
+++ day6/main.hs
@@ -13,6 +13,20 @@ solveProblem ("+":rest) = sum $ map read rest
 main :: IO ()
 main = do
   fileName <- (!!0) <$> getArgs
-  problems <- extractInputs <$> readFile fileName
-  let sltn1 = sum . map solveProblem $ problems
-  print sltn1
+  contents <- readFile fileName
+  let sltn1 = sum . map solveProblem $ extractInputs contents
+  let sltn2 = sum . map solveProblem $ extractInputsCephStyle contents
+  print (sltn1, sltn2)
+
+-- we need a function to split the numbers up that will be aware of the alignment of each number in
+-- it's respective column
+breakAwareSplit :: [String] -> [[String]]
+breakAwareSplit [] = []
+breakAwareSplit strings = block : breakAwareSplit (drop 1 rest)
+  where (block, rest) = break (all (==' ')) strings 
+
+extractInputsCephStyle :: String -> [Problem]
+extractInputsCephStyle input = zipWith prependOp ops blocks
+  where lns = lines input
+        (blocks, ops) = (breakAwareSplit . transpose . init $ lns, filter (/=' ') . last $ lns)
+        prependOp op block = [op] : block
