fe4e2ed1ad99a0f88c5e9056677ecc087fcf7257
BraeTroutman <btroutma@redhat.com>
2025-12-11 10:42:09 -0500

    add day8 part 2 solution

--- day8/main.hs
+++ day8/main.hs
@@ -1,7 +1,7 @@
 import System.Environment (getArgs)
 import qualified Data.Set as Set
 import qualified Data.Map as Map
-import Data.Array ((!), (//), Array (..), listArray, elems)
+import Data.Array ((!), (//), Array (..), listArray, elems, bounds)
 import Control.Monad.State
 import Data.List (sortOn, sort, sortBy)
 import Data.Ord (Down (..), comparing)
@@ -13,7 +13,7 @@ data Point = Point { x :: Float
 
 type Circuit = Set.Set Point
 
-data ProgState = ProgState { _sets :: Array Int Circuit, _pointToSetID :: Map.Map Point Int} deriving Show
+data ProgState = ProgState { _sets :: Array Int Circuit, _pointToSetID :: Map.Map Point Int, _largestSetId :: Int} deriving Show
 
 main :: IO ()
 main = do
@@ -21,28 +21,37 @@ main = do
   contents <- readFile fileName
   let points = map lineToPoint (lines contents)
   let initial = initState points
-  let pairs' = take 1000 $ sortOn (uncurry distance) (pairs points)
-  let finalState = execState (mapM step pairs') initial
-  let sltn = (product . take 3 . sortBy (comparing Data.Ord.Down) . elems . fmap Set.size . _sets) finalState
-  print sltn
+  let allPairs = sortOn (uncurry distance) (pairs points)
+  let firstThousand = take 1000 allPairs
+  let sltn1FinalState = execState (mapM step firstThousand) initial
+  let (Just (Point x _ _, Point x' _ _)) = head . dropWhile (==Nothing) . evalState (mapM step allPairs) $ initial
+  let sltn1 = (product . take 3 . sortBy (comparing Data.Ord.Down) . elems . fmap Set.size . _sets) sltn1FinalState
+  print (sltn1, round x * round x')
 
 updateStateWithPoints :: (Point, Point) -> ProgState -> ProgState
-updateStateWithPoints (p1, p2) (ProgState sets pointToSetIdMap) = ProgState newSets newMap
+updateStateWithPoints (p1, p2) (ProgState sets pointToSetIdMap currentLargestSet) = ProgState newSets newMap newLargestSet
       where setOf = (pointToSetIdMap Map.!)
             s1 = setOf p1
             s2 = setOf p2
-            newSets = if s1 == s2 then sets else sets // [(s1, Set.union (sets ! s1) (sets ! s2))]
+            newSets = if s1 == s2 then sets else sets // [(s1, Set.union (sets ! s1) (sets ! s2)), (s2, Set.empty)]
             newMap  = if s1 == s2 then pointToSetIdMap else setIdOfPointsTo (Set.toList $ sets ! s2) s1
+            newLargestSet
+              | Set.size (newSets ! s1) > Set.size (newSets ! currentLargestSet) = s1
+              | otherwise = currentLargestSet
             setIdOfPointsTo pts setId = foldr (`Map.insert` setId) pointToSetIdMap pts
 
-step :: (Point, Point) -> State ProgState ()
-step pt = modify (updateStateWithPoints pt)
+step :: (Point, Point) -> State ProgState (Maybe (Point, Point))
+step pt = do
+  modify (updateStateWithPoints pt)
+  ProgState sets _ maxSetId <- get
+  let (_, n) = bounds sets
+  if Set.size (sets ! maxSetId) == n then return (Just pt) else return Nothing
 
 mkCircuit :: Point -> Circuit
 mkCircuit = Set.singleton
 
 initState :: [Point] -> ProgState
-initState pts = ProgState (listArray (1, length pts) (map mkCircuit pts)) (Map.fromList $ zip pts [1..])
+initState pts = ProgState (listArray (1, length pts) (map mkCircuit pts)) (Map.fromList $ zip pts [1..]) 1
 
 distance :: Point -> Point -> Float
 distance (Point x y z) (Point x' y' z') = sqrt $ (x - x')^2 + (y - y')^2 + (z - z')^2
